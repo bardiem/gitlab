944a984927dbf0d493ee5cba1f74a474ba089285
bardiem <38584359+bardiem@users.noreply.github.com>
2019-03-28 01:12:28 +0200

    Add files via upload

---
+++ HelloWorldGit/HelloWorldGit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HelloWorldGit
+{
+    namespace HelloWorldGit
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                FileStream file = new FileStream("file.txt", FileMode.OpenOrCreate);
+                StreamWriter writer = new StreamWriter(file); 
+                writer.WriteLine(DateTime.Now.ToString());
+                StreamReader reader = new StreamReader(file);
+                string lastLaunch = "";
+                while (!reader.EndOfStream)
+                {
+                    lastLaunch = reader.ReadLine();
+                }
+                if (lastLaunch == "")
+                    Console.WriteLine("Файл пустий");
+                Console.WriteLine(lastLaunch);
+                writer.Close();
+                reader.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
